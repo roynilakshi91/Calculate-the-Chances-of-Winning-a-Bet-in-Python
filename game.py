32ab4e58ad75bc24f29157f0d7f13a33f480b33f
roynilakshi91 <82709191+roynilakshi91@users.noreply.github.com>
2022-03-31 20:56:45 -0700

    Update game.py

--- game.py
+++ game.py
@@ -88,3 +88,53 @@ plt.plot(random_walk)
 
 # Show the plot
 plt.show()
+
+#Distribution
+#To get an idea about how big your chances are of reaching 60 steps, you can repeatedly simulate the random walk and collect the results.
+# Initialize all_walks (don't change this line)
+all_walks = []
+
+# Simulate random walk 10 times
+for i in range(10) :
+
+    # Code from before
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+
+    # Append random_walk to all_walks
+    all_walks.append(random_walk)
+
+# Print all_walks
+print(all_walks)
+
+
+# numpy and matplotlib imported, seed set.
+
+
+# Convert all_walks to NumPy array: np_aw
+np_aw = np.array(all_walks)
+
+# Plot np_aw and show
+plt.plot(np_aw)
+plt.show()
+
+# Clear the figure
+plt.clf()
+
+# Transpose np_aw: np_aw_t
+#Transposing the 2D NumPy array was crucial; otherwise Python misunderstood.
+np_aw_t = np.transpose(np_aw)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
