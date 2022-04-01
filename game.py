f529a193cd713e16eb620a831b2a0c21d87a7891
roynilakshi91 <82709191+roynilakshi91@users.noreply.github.com>
2022-03-31 22:37:45 -0700

    Update game.py

--- game.py
+++ game.py
@@ -138,3 +138,65 @@ np_aw_t = np.transpose(np_aw)
 # Plot np_aw_t and show
 plt.plot(np_aw_t)
 plt.show()
+
+
+# Simulate random walk 250 times
+all_walks = []
+for i in range(250) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+
+        # Implement clumsiness
+        if np.random.rand() <= 0.001 :
+            step = 0
+
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+plt.plot(np_aw_t)
+plt.show()
+
+#What are the odds that you'll reach 60 steps high on the Empire State Building?
+# Simulate random walk 500 times
+all_walks = []
+for i in range(500) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        if np.random.rand() <= 0.001 :
+            step = 0
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1,:]
+
+# Plot histogram of ends, display plot
+plt.hist(ends)
+plt.show()
+
+#what's the estimated chance that you'll reach at least 60 steps high if you play this Empire State Building game?
+np.mean(ends>=60)
+0.784
+#78.4%
+# Seems like I have a pretty high chance of winning the bet!
